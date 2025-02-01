55bd3a8915c6815decfcdb7e4e6ece1ba408eaea
VineetKundu <kunduvineet6@gmail.com>
2025-02-01 18:34:17 +0530

    AOP-XSD1

--- AOP-XSD1/src/main/java/in/vk/main/App.java
+++ AOP-XSD1/src/main/java/in/vk/main/App.java
@@ -1,13 +1,27 @@
 package in.vk.main;
 
-/**
- * Hello world!
- *
- */
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import in.vk.services.BankTransaction;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	ApplicationContext context = new ClassPathXmlApplicationContext("/in/vk/resources/applicationContext.xml");
+    	
+    	BankTransaction bt = (BankTransaction) context.getBean("banktransId");
+    	
+    	bt.transactionUsingCreditCard();
+    	System.out.println();
+    	
+    	bt.transactionUsingInternetBanking();
+    	System.out.println();
+    	
+    	bt.transactionUsingUPI();
+    	System.out.println();
+    	
+    	bt.transactionUsingWallet();
     }
 }
