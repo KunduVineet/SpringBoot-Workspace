5d0d3a497ed07df9358f9f0d7a6c1a90e35dba4f
VineetKundu <kunduvineet6@gmail.com>
2025-02-01 14:04:55 +0530

    AOP-XSD1

---
+++ AOP-XSD1/src/main/java/in/vk/aspects/LoggingAspect.java
@@ -0,0 +1,9 @@
+package in.vk.aspects;
+
+public class LoggingAspect {
+	
+	public void myLoggingBefore() {
+		System.out.println("----------Before Logging-----------");
+	}
+
+}
---
+++ AOP-XSD1/src/main/java/in/vk/services/BankTransaction.java
@@ -0,0 +1,20 @@
+package in.vk.services;
+
+public class BankTransaction {
+	public void transactionUsingUPI() {
+		System.out.println("Business logic for UPI Transaction");
+	}
+	
+	public void transactionUsingWallet() {
+		System.out.println("Business logic for Wallet Transaction");
+	}
+	
+	public void transactionUsingInternetBanking() {
+		System.out.println("Business logic for Internet Banking Transaction");
+	}
+	
+	public void transactionUsingCreditCard() {
+		System.out.println("Business logic for Credit Card Transaction");
+	}
+
+}
