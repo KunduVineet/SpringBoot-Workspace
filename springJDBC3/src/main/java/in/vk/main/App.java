50a9f3c5fa7946ed47388d090cb071f67fdc6682
VineetKundu <kunduvineet6@gmail.com>
2025-02-11 12:44:42 +0530

    NamedParameterJdbcTemplate - MapSqlParameterSource

--- springJDBC3/src/main/java/in/vk/main/App.java
+++ springJDBC3/src/main/java/in/vk/main/App.java
@@ -11,7 +11,6 @@ public class App
 {
     public static void main( String[] args )
     {
-    	
     	MapSqlParameterSource params = new MapSqlParameterSource();
     	params.addValue("name", "Vineet");
     	params.addValue("email", "advn@gmail.com");
