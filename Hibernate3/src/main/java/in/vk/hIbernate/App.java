db76cf552b41d23721ea4a4bb56a707ab7266ccb
VineetKundu <kunduvineet6@gmail.com>
2025-02-15 16:03:13 +0530

    Hibernate : Id (Auto-Increment)

---
+++ Hibernate3/src/main/java/in/vk/hIbernate/App.java
@@ -0,0 +1,41 @@
+package in.vk.hIbernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import in.vk.beans.Employee;
+
+public class App {
+	public static void main(String[] args) {
+		Employee std = new Employee();
+		std.setEmpcity("Gurugram");
+		std.setEmpname("Vineet Kundu");
+		std.setEmpdept("IT and Infrastructure");
+
+		Configuration cfg = new Configuration();
+		cfg.configure("in/vk/resources/hibernate.cfg.xml");
+
+		SessionFactory sessionFactory = cfg.buildSessionFactory();
+		Session session = sessionFactory.openSession();
+
+		Transaction transaction = session.beginTransaction();
+
+		try {
+			session.save(std);
+			System.out.println("Success");
+
+			transaction.commit();
+
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("fail");
+			transaction.rollback();
+		} finally {
+			session.close();
+			sessionFactory.close();
+		}
+
+	}
+}
