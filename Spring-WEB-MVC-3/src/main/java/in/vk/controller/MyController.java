2df408b0ad0b63cb7d94715099998757034b6b7d
VineetKundu <kunduvineet6@gmail.com>
2025-03-03 23:26:42 +0530

    Form Data Submission- ModelAndView

--- Spring-WEB-MVC-3/src/main/java/in/vk/controller/MyController.java
+++ Spring-WEB-MVC-3/src/main/java/in/vk/controller/MyController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -15,12 +16,23 @@ public class MyController {
 	public String open() {
 		return "hello";
 	}
+//	
+//	@PostMapping("/myForm")
+//	public String getFormName(HttpServletRequest req, HttpServletResponse resp, Model model) {
+//		
+//		String myname = req.getParameter("name");
+//		model.addAttribute("model_name", myname);
+//		return "hello";
+//	}
 	
 	@PostMapping("/myForm")
-	public String getFormName(HttpServletRequest req, HttpServletResponse resp, Model model) {
+	public ModelAndView getFormname(HttpServletRequest req, HttpServletResponse resp) {
+	String myname = req.getParameter("name");
+
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("model_name", myname);
+		mav.setViewName("hello");
 		
-		String myname = req.getParameter("name");
-		model.addAttribute("model_name", myname);
-		return "hello";
+		return mav;
 	}
 }
