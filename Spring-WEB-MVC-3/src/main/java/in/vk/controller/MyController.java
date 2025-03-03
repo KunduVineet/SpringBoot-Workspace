5d49ccf2603f0a8338844478e28d88d1ebd26dcf
VineetKundu <kunduvineet6@gmail.com>
2025-03-03 23:35:16 +0530

    Form Data Submission- ModelAndView

--- Spring-WEB-MVC-3/src/main/java/in/vk/controller/MyController.java
+++ Spring-WEB-MVC-3/src/main/java/in/vk/controller/MyController.java
@@ -1,7 +1,6 @@
 package in.vk.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
