ec69d0a37d6ea3eab744eef93e907526f7570e56
VineetKundu <kunduvineet6@gmail.com>
2025-03-04 10:46:41 +0530

    HTTPSession

---
+++ Spring-WEB-MVC-4-HttpSession/src/main/java/in/vk/controller/LoginController.java
@@ -0,0 +1,27 @@
+package in.vk.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String openLoginPage() {
+        return "login";  // Returns login.jsp (or login.html depending on your view resolver)
+    }
+
+    @PostMapping("/loginform")
+    public String loginForm(@RequestParam("email") String email, @RequestParam("password") String password, Model model) {
+
+        if(email.equals("kunduvineet6@gmail.com") && password.equals("vineet")) {
+            return "profile";  // Redirect to profile.jsp
+        } else {
+        	model.addAttribute("login_error","Email and password didn't matched");
+            return "login";  // Stay on login.jsp
+        }
+    }
+}
---
+++ Spring-WEB-MVC-4-HttpSession/src/main/webapp/index.jsp
@@ -0,0 +1,7 @@
+<html>
+<body>
+    <h2>Hello in the World! of Spring Boot</h2>
+
+    <a href="login">Login</a>
+</body>
+</html>
