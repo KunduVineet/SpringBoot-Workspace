4b9ba0b599a347b83aebdf3ae8686b91493bd86f
VineetKundu <kunduvineet6@gmail.com>
2025-03-04 10:53:09 +0530

    HTTPSession- Session

--- Spring-WEB-MVC-4-HttpSession/src/main/java/in/vk/controller/LoginController.java
+++ Spring-WEB-MVC-4-HttpSession/src/main/java/in/vk/controller/LoginController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import jakarta.servlet.http.HttpSession;
+
 @Controller
 public class LoginController {
 
@@ -15,9 +17,11 @@ public class LoginController {
     }
 
     @PostMapping("/loginform")
-    public String loginForm(@RequestParam("email") String email, @RequestParam("password") String password, Model model) {
+    public String loginForm(@RequestParam("email") String email, @RequestParam("password") String password, Model model, HttpSession session) {
 
         if(email.equals("kunduvineet6@gmail.com") && password.equals("vineet")) {
+        	session.setAttribute("session_name", "Vineet Kundu");
+        	session.setAttribute("session_gender", "Male");
             return "profile";  // Redirect to profile.jsp
         } else {
         	model.addAttribute("login_error","Email and password didn't matched");
--- Spring-WEB-MVC-4-HttpSession/src/main/webapp/WEB-INF/views/profile.jsp
+++ Spring-WEB-MVC-4-HttpSession/src/main/webapp/WEB-INF/views/profile.jsp
@@ -1,13 +1,17 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+    pageEncoding="UTF-8" isELIgnored="false"%>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title>Profile Page</title>
 </head>
 <body>
 <h2>Profile Page this side</h2>
+
+<h2>Welcome ${session_name}</h2>
+
+<h2>Gender ${session_gender}</h2>
 
 </body>
 </html>
