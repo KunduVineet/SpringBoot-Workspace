a167f56a10fef3ab4cf782ccd6901439e9766824
VineetKundu <kunduvineet6@gmail.com>
2025-03-21 18:06:07 +0530

    SpringBoot - JDBC

--- SpringBootJDBC1/src/main/java/in/vk/controller/MyController.java
+++ SpringBootJDBC1/src/main/java/in/vk/controller/MyController.java
@@ -2,7 +2,8 @@ package in.vk.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MyController {
@@ -12,8 +13,13 @@ public class MyController {
 		return "index";
 	}
 	
-	@RequestMapping("/loginPage")
+	@GetMapping("/loginPage")
 	public String openLoginPage() {
 		return "Login";
+	}
+	
+	@PostMapping("/LoginForm")
+	public String Login(@RequestParam("username") String email, @RequestParam ("password")String password) {
+		return "";
 	}
 }
