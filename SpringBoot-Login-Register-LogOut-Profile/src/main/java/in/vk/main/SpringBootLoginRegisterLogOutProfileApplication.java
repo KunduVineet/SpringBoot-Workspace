60e64c6d87998724c37b45ae37fd871bd9548303
VineetKundu <kunduvineet6@gmail.com>
2025-03-23 13:39:11 +0530

    SpringBoot JDBC - Login, LogOut, Registeration, Profile Page

---
+++ SpringBoot-Login-Register-LogOut-Profile/src/main/java/in/vk/main/SpringBootLoginRegisterLogOutProfileApplication.java
@@ -0,0 +1,13 @@
+package in.vk.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLoginRegisterLogOutProfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLoginRegisterLogOutProfileApplication.class, args);
+	}
+
+}
