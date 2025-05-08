b59bf0ba206cf8c59bdcc2384a05affc32a668d1
VineetKundu <kunduvineet6@gmail.com>
2025-05-08 19:05:38 +0530

    Error Sucessfully Resolved - Custom Complex Queries using JPA

--- Spring-Data-JPA-3/src/main/java/in/vk/main/repository/UserRepository.java
+++ Spring-Data-JPA-3/src/main/java/in/vk/main/repository/UserRepository.java
@@ -4,17 +4,29 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import in.vk.main.entities.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
 	//Custom Method to find the user by email according to jpa standards. 
 	//ReturnType findBy<Property>(Type value);
-	Optional<User> findByEmail(String value);
-	Optional<List<User>> findByName(String name);
+	Optional<User> findByEmailOrderByNameAsc(String email);
+	List<User> findByName(String name);
 	
-	Optional<List<User>> findByNameAndEmail(String name, String email);
+	List<User> findByNameAndEmail(String name, String email);
 	
-	Optional<List<User>> findByNameOrEmail(String name, String email);
+	List<User> findByNameOrEmail(String name, String email);
+	
+	int countByName(String name);
+	
+	boolean existsByEmail(String email);
+	
+	List<User> findByNameContaining(String nameKeyword);
+	
+	List<User> findByNameLike(String pattern);
+	
+
 }
---
+++ Spring-Data-JPA-3/src/test/java/in/vk/main/repo/UserRepo.java
@@ -0,0 +1,29 @@
+package in.vk.main.repo;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import in.vk.main.entities.User;
+import in.vk.main.repository.UserRepository;
+
+@SpringBootTest
+public class UserRepo {
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	@Test
+	public void repoTest1() {
+	Optional<User> userOptional = userRepository.findByEmailOrderByNameAsc("abc@gmail.com");
+	
+	userOptional.ifPresent(user ->{
+		System.out.println(user.getUser_id());
+		System.out.println(user.getName());
+		System.out.println(user.getEmail());
+		
+	});
+	}
+}
