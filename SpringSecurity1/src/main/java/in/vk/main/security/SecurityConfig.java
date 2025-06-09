db611472a89d10ad273834e781101ba2f7606479
VineetKundu <kunduvineet6@gmail.com>
2025-06-09 17:12:46 +0530

    Spring Security- API Secure

--- SpringSecurity1/src/main/java/in/vk/main/security/SecurityConfig.java
+++ SpringSecurity1/src/main/java/in/vk/main/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package in.vk.main.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.SecurityFilterChain;
@@ -18,7 +19,15 @@ public class SecurityConfig {
 			httpRequest.requestMatchers("/admin/result").authenticated();
 			httpRequest.requestMatchers("/admin/get-results").authenticated();
 			httpRequest.anyRequest().permitAll();
+			
 		});
+		
+		http.formLogin(formLogin ->{
+			formLogin.loginPage("/login")
+			.successForwardUrl("/admin/add-results")
+			.failureForwardUrl("/user-login")
+			.permitAll(true);
+		}).logout(Customizer.withDefaults());
 		return build;
 		
 	}
