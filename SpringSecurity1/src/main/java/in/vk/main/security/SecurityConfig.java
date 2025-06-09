b4414d3a3b2a2e381f52e1b97e857cc6961f8a8d
VineetKundu <kunduvineet6@gmail.com>
2025-06-09 17:00:42 +0530

    Spring Security

--- SpringSecurity1/src/main/java/in/vk/main/security/SecurityConfig.java
+++ SpringSecurity1/src/main/java/in/vk/main/security/SecurityConfig.java
@@ -1,5 +1,26 @@
 package in.vk.main.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfig {
+	
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+		DefaultSecurityFilterChain build = http.build();
+		
+		//Springboot API security 
+		http.authorizeHttpRequests(httpRequest -> {
+			httpRequest.requestMatchers("/admin/result").authenticated();
+			httpRequest.requestMatchers("/admin/get-results").authenticated();
+			httpRequest.anyRequest().permitAll();
+		});
+		return build;
+		
+	}
 
 }
