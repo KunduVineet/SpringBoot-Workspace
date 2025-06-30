d7c69c7e389ba9269c2bd48f5dfef4340adecf85
VineetKundu <kunduvineet6@gmail.com>
2025-06-30 18:16:09 +0530

    Spring Security - Role Based access

--- Spring-Security/src/main/java/in/vk/main/GreetingsController.java
+++ Spring-Security/src/main/java/in/vk/main/GreetingsController.java
@@ -1,5 +1,6 @@
 package in.vk.main;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,18 @@ public class GreetingsController {
 	@GetMapping("/hello")
 	public String sayHello() {
 		return "Hello";
+	}
+	
+	@PreAuthorize("hasRole('USER')")
+	@GetMapping("/user")
+	public String userEndpoint() {
+		return "User can acess this";
+	}
+	
+	@PreAuthorize("hasRole('ADMIN')")
+	@GetMapping("/admin")
+	public String adminEndpoint() {
+		return "Admin can acess this";
 	}
 
 }
--- Spring-Security/src/main/java/in/vk/main/SecurityConfig.java
+++ Spring-Security/src/main/java/in/vk/main/SecurityConfig.java
@@ -1,24 +1,49 @@
 package in.vk.main;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 	
 	@Bean
 	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception{
-		http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+		http.authorizeHttpRequests((requests) -> requests.requestMatchers("/h2-console/**").permitAll()
+				.anyRequest().authenticated());
 		http.sessionManagement(session ->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 		http.httpBasic(withDefaults());
 		return http.build();
+	}
+	
+	//in-memory authentication
+	
+	@Bean
+	public UserDetailsService userdetailsService() {
+		
+		UserDetails user1 = User.withUsername("user1")
+				.password("{noop}password1")
+				.roles("USER")
+				.build();
+		
+		UserDetails admin = User.withUsername("admin")
+				.password("{noop}admin1")
+				.roles("ADMIN")
+				.build();
+				
+		return new InMemoryUserDetailsManager(user1,admin);
 	}
 
 }
