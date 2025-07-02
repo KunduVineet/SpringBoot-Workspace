47d9e861d714d15d9c36a2b7c8762ab836fbb242
VineetKundu <kunduvineet6@gmail.com>
2025-07-02 15:42:13 +0530

    BCrypt password Encoder

--- Spring-Security/src/main/java/in/vk/main/SecurityConfig.java
+++ Spring-Security/src/main/java/in/vk/main/SecurityConfig.java
@@ -3,10 +3,9 @@ package in.vk.main;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 import javax.sql.DataSource;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -15,7 +14,8 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -24,50 +24,43 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableMethodSecurity
 public class SecurityConfig {
 
-    private final UserDetailsService userdetailsService;
-	
-	@Autowired
-	DataSource dataSource;
+    private final UserDetailsService userDetailsService;
 
-    SecurityConfig(UserDetailsService userdetailsService) {
-        this.userdetailsService = userdetailsService;
+    public SecurityConfig(@Lazy UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
     }
-	
-	@Bean
-	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception{
-		http.authorizeHttpRequests((requests) -> requests.requestMatchers("/h2-console/**").permitAll()
-				.anyRequest().authenticated());
-		http.sessionManagement(session ->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-		http.httpBasic(withDefaults());
-		http.headers(headers ->
-				headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
-		http.csrf(csrf ->csrf.disable());
-		return http.build();
-	}
-	
-	//in-memory authentication
-	
-	@Bean
-	public UserDetailsService userdetailsService() {
-		
-		UserDetails user1 = User.withUsername("user1")
-				.password("{noop}password1")
-				.roles("USER")
-				.build();
-		
-		UserDetails admin = User.withUsername("admin")
-				.password("{noop}admin1")
-				.roles("ADMIN")
-				.build();
-		
-		
-		//using Db to store credentials
-		JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager(dataSource);
-		userDetailsManager.createUser(user1);
-		userDetailsManager.createUser(admin);
-		return userDetailsManager;
-				
-//		return new InMemoryUserDetailsManager(user1,admin);
-	}
 
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> requests
+                .requestMatchers("/h2-console/**").permitAll()
+                .anyRequest().authenticated());
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.httpBasic(withDefaults());
+        http.headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
+        http.csrf(csrf -> csrf.disable());
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        UserDetails user1 = User.withUsername("user1")
+                .password(passwordEncoder().encode("password1"))
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.withUsername("admin")
+                .password(passwordEncoder().encode("admin1"))
+                .roles("ADMIN")
+                .build();
+
+        JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager(dataSource);
+        userDetailsManager.createUser(user1);
+        userDetailsManager.createUser(admin);
+        return userDetailsManager;
+    }
+    
+    public PasswordEncoder passwordEncoder() {
+    	return new BCryptPasswordEncoder();
+    }
 }
