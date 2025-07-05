8f50163829a1c539ffabef64488de2b4f502c74f
VineetKundu <kunduvineet6@gmail.com>
2025-07-05 19:29:17 +0530

    Custom Authentication Filter

---
+++ Spring-Security/src/main/java/in/vk/main/jwt/AuthTokenFilter.java
@@ -0,0 +1,34 @@
+package in.vk.main.jwt;
+
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class AuthTokenFilter extends OncePerRequestFilter{
+	
+	@Autowired
+	private JwtUtils jwtutils;
+
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
+	private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
+
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
