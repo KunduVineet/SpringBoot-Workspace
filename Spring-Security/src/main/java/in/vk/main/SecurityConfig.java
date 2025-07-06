a5319fe7dfb55695b4f7eb6663b9948a41e112ea
VineetKundu <kunduvineet6@gmail.com>
2025-07-06 17:10:04 +0530

    JWT Token Sign In

--- Spring-Security/src/main/java/in/vk/main/SecurityConfig.java
+++ Spring-Security/src/main/java/in/vk/main/SecurityConfig.java
@@ -1,11 +1,13 @@
 package in.vk.main;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
 import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -18,28 +20,48 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import in.vk.main.jwt.AuthEntryPointJwt;
+import in.vk.main.jwt.AuthTokenFilter;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
 
+    private final AuthTokenFilter authenticationJwttokenFilter;
+
     private final UserDetailsService userDetailsService;
 
-    public SecurityConfig(@Lazy UserDetailsService userDetailsService) {
+    @Autowired
+    DataSource dataSource;
+    
+    @Autowired
+    private AuthEntryPointJwt unauthorizedHandler;
+    
+    @Bean
+    public AuthTokenFilter authenticationJwttokenFilter() {
+    	return new AuthTokenFilter();
+    }
+    
+    public SecurityConfig(@Lazy UserDetailsService userDetailsService, AuthTokenFilter authenticationJwttokenFilter) {
         this.userDetailsService = userDetailsService;
+        this.authenticationJwttokenFilter = authenticationJwttokenFilter;
     }
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((requests) -> requests
                 .requestMatchers("/h2-console/**").permitAll()
+                .requestMatchers("/api/signin").permitAll()
                 .anyRequest().authenticated());
         http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-        http.httpBasic(withDefaults());
+//        http.httpBasic(withDefaults());
+        http.exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler));
         http.headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
         http.csrf(csrf -> csrf.disable());
-        return http.build();
+        http.addFilterBefore(authenticationJwttokenFilter, UsernamePasswordAuthenticationFilter.class);        return http.build();
     }
 
     @Bean
@@ -60,7 +82,13 @@ public class SecurityConfig {
         return userDetailsManager;
     }
     
+    @Bean
     public PasswordEncoder passwordEncoder() {
     	return new BCryptPasswordEncoder();
+    }
+    
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration builder) throws Exception {
+    	return builder.getAuthenticationManager();
     }
 }
