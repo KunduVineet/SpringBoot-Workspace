00e07311741a90aa63eb794053ea1572112df89b
VineetKundu <kunduvineet6@gmail.com>
2025-08-05 16:04:36 +0530

    OAuth2

---
+++ OAuth2/src/main/java/in/vk/main/OAuth2Application.java
@@ -0,0 +1,13 @@
+package in.vk.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAuth2Application.class, args);
+	}
+
+}
